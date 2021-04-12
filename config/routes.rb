10b5f14262583b3b0bc5df8975db2503cc9c4b2c
claire <clairebontempo@gmail.com>
2021-04-12 11:56:35 -0700

    creates signup route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :programs
   resources :exercises
   resources :users, only: [:create]
+  get '/signup', to: 'users#create'
   post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
