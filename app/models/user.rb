688ea4f5c6b43ec7cfabad5ecf955f289f2b2576
claire <clairebontempo@gmail.com>
2021-04-12 10:58:46 -0700

    adds validations to email attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :user_programs
     has_many :programs, through: :user_programs
     has_secure_password
+    validates :email, uniqueness: { case_sensitive: false }
 end
