185c3e426c3b7dd86e4514b4ee6e4c2a893ad5b2
claire <clairebontempo@gmail.com>
2021-04-19 10:47:48 -0700

    adds validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :programs, through: :user_programs
     has_secure_password
     validates :email, uniqueness: { case_sensitive: false }
+    validates :name, :password, presence: true
 end
