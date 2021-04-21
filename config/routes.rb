808008d5aa4efc003a61ef276b466dc60b15d42f
claire <clairebontempo@gmail.com>
2021-04-21 10:10:09 -0700

    adds auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   resources :program_exercises
   resources :programs
   resources :exercises
-  resources :users, only: [:create]
+  # resources :users, only: [:create]
   post '/signup', to: 'users#create'
   post '/login', to: 'auth#create'
+  get '/auto_login', to: 'auth#auto_login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
